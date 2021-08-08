77a0b48e8afc765eb47c69c58598d5d9875a80f4
YoucefMesselem <messelemy@gmail.com>
2021-08-08 21:58:04 +0400

    Created a new report model using the xls Added some decorations to the xls Added an image by importing the base 64 package as well as the io package

--- report/patient_card_xls.py
+++ report/patient_card_xls.py
@@ -1,18 +1,30 @@
 from odoo import models
+import base64
+import io
+
 
 class PatientCardXlsx(models.AbstractModel):
     _name = 'report.om_hospital.report_patient_card_xlsx'
     _inherit = 'report.report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, patients):
-
-        sheet = workbook.add_worksheet('PATIENT ID CARD')
         bold = workbook.add_format({'bold': True})
-        row = 3
-        col = 3
-        sheet.set_column("D:E", 30)
+        format_align = workbook.add_format({'bold': True,
+                                            'align': 'center',
+                                            'bg_color': 'yellow'})
+
         for obj in patients:
+            row = 3
+            col = 3
+            sheet = workbook.add_worksheet(obj.name)
+            sheet.set_column("D:D", 12)
+            sheet.set_column("E:E", 13)
+            sheet.merge_range(row, col, row, col+1, 'ID CARD', format_align)
             row += 1
+            if obj.image:
+                patient_image = io.BytesIO(base64.b64decode(obj.image))
+                sheet.insert_image(row, col, "staff.png", {'image_data': patient_image, 'x_scale': 0.5, 'y_scale': 0.5})
+                row += 7
             # One sheet by patient
             sheet.write(row, col, "Name", bold)
             sheet.write(row, col+1, obj.name)
@@ -22,3 +34,4 @@ class PatientCardXlsx(models.AbstractModel):
             row += 1
             sheet.write(row, col, "Reference", bold)
             sheet.write(row, col+1, obj.reference)
+            row += 2
