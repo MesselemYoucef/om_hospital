235c0f4013465829ff76e0b58dd846fba40f4017
Youcef Messelem <messelemy@gmail.com>
2021-09-22 15:19:48 +0400

    [completed] how to create pdf report from wizard

--- wizard/appointment_report.py
+++ wizard/appointment_report.py
@@ -4,17 +4,23 @@ class AppointmentReport(models.TransientModel):
     _name = "appointment.report.wizard"
     _description = "Wizard that falicitates retrieving appointments"
 
-    date_from = fields.Date(string="From", required=False)
-    date_to = fields.Date(string="To", required=False)
+    date_from = fields.Datetime(string="From", required=False)
+    date_to = fields.Datetime(string="To", required=False)
     patient_id = fields.Many2one("hospital.patient", string="Patient Name", required=False)
 
     def action_print_report(self):
-        patient_id = self.read()[0].get('patient_id')
+        domain = []
+        patient_id = self.patient_id
         if patient_id:
-            domain += [
-                ('patient_id', '=', patient_id[0])
-            ]
-        appointments = self.env['hospital.appointment'].search_read([('patient_id', '=', self.patient_id.id)])
+            domain += [('patient_id', '=', patient_id.id)]
+        date_from = self.date_from
+        date_to = self.date_to
+        if date_from:
+            domain += [('appointment_date', '>=', date_from)]
+        if date_to:
+            domain += [('appointment_date', '<=', date_to)]
+
+        appointments = self.env['hospital.appointment'].search_read(domain)
         data = {
             'form': self.read()[0],
             'appointments': appointments
